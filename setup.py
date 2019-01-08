97cde8017f1389b2e0065628c6514488bf0260ac
Stefan Bethke <stb@lassitu.de>
2019-01-08 14:03:55 +0100

    Correct URL and description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='tcpatmodem',
       version='0.1',
-      description='Interpret AT commands received over a TCP connection',
-      url='http://github.com/stblassitude/tcpatmodem',
+      description='AT command interpreter and TCP dialler',
+      url='https://github.com/stblassitude/tcpatmodem',
       author='Stefan Bethke',
       author_email='stb@lassitu.de',
       license='MIT',
